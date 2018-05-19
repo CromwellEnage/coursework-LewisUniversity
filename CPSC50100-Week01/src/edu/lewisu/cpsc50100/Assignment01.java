6fc04ff8cb6501c4d722fe1ef7c04cbf5a73b9db
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-05-19 17:27:20 -0400

    Input File Added
    Scores.txt added to assignment root directory.

--- CPSC50100-Week01/src/edu/lewisu/cpsc50100/Assignment01.java
+++ CPSC50100-Week01/src/edu/lewisu/cpsc50100/Assignment01.java
@@ -1,33 +1,53 @@
 package edu.lewisu.cpsc50100;
 
+import java.io.*;
+
 /*
 * @author Cromwell D. Enage
 */
 public class Assignment01 {
 
 	/*
-	* This "algorithm" parses the command line arguments for its input values.
-	* The first argument determines how many test grades there are.
+	* This "algorithm" parses the file specified by args[0] for its inputs.
+	* The first line determines how many test grades there are.
 	* All other grades are assignment grades.
-	* Each following pair of arguments consists of a grade and its weight.
+	* Each following pair of lines consists of a grade and its weight.
 	* The output is printed to the console rather than returned by the function.
 	*/
 	public static void main(String[] args) {
 		Averager test_averager = new Averager();
 		Averager assignment_averager = new Averager();
-		int test_count = Integer.parseInt(args[0]) * 2;
-		int i = 1;
 
-		while (i < test_count)
+		try
 		{
-			test_averager.add(Integer.parseInt(args[i]) * Double.parseDouble(args[i + 1]));
-			i += 2;
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(new FileInputStream(args[0]))
+					);
+			int test_count = Integer.parseInt(reader.readLine()) * 2;
+
+			for (int i = 1; i < test_count; i += 2)
+			{
+				test_averager.add(
+						Integer.parseInt(reader.readLine()) *
+						Double.parseDouble(reader.readLine())
+						);
+			}
+
+			String line = null;
+
+			while ((line = reader.readLine()) != null)
+			{
+				int grade = Integer.parseInt(line);
+				line = reader.readLine();
+				if (line == null) break;
+				double weight = Double.parseDouble(line);
+				assignment_averager.add(grade * weight);
+			}
+
+			reader.close();
 		}
-
-		while (i + 1 < args.length)
+		catch (IOException e)
 		{
-			assignment_averager.add(Integer.parseInt(args[i]) * Double.parseDouble(args[i + 1]));
-			i += 2;
 		}
 
 		MasterAverager master_averager = new MasterAverager();
